903eaaaf0e3c2136254f2d8c772f268c24df9518
Facundo Estevez <fmestevez@gmail.com>
2014-08-26 18:22:44 -0300

    Facundo Estevez - PODVI - Basic run animation

--- public/js/play.js
+++ public/js/play.js
@@ -1,10 +1,14 @@
 var playState = {
 
 	create: function() { 
-
+        this.player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+        this.player.anchor.setTo(0.5, 0.5);
+        this.player.animations.add('run', [0, 1], 8, true);
+        this.player.animations.add('jump', [2, 3], 8, true);
+        game.physics.arcade.enable(this.player);
 	},
 
 	update: function() {
-
+        this.player.animations.play('run');
 	},
 };
