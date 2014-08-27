befdc85d5ffa310e359e5c09281aa1bbf5737585
Facundo Estevez <fmestevez@gmail.com>
2014-08-27 17:19:24 -0300

    Facu | Diego | Mema - PODVI - Define controls, acceleration and drag.

--- public/js/play.js
+++ public/js/play.js
@@ -1,28 +1,58 @@
 var playState = {
 
 	create: function() { 
-        this.player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+        // Create player
+        this.player = game.add.sprite(50, game.world.centerY, 'player');
         this.player.anchor.setTo(0.5, 0.5);
+        
+        // Add player's animations
         this.player.animations.add('run', [0, 1], 8, true);
         this.player.animations.add('jump', [2, 3], 8, true);
+        this.player.frame = 0;
+        
+        // Enables player to have physics
         game.physics.arcade.enable(this.player);
         this.player.body.gravity.y = 400;
+        game.physics.enable(this.player, Phaser.Physics.ARCADE);
         
+        // Creates world (loads tilemap)
         this.createWorld();
+        
+        // Adds new key to control the player
+        this.keys = {
+            space: game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR),
+        };
+        
+        this.keys.space.onDown.add(this.movePlayer, this);
 	},
 
 	update: function() {
         game.physics.arcade.collide(this.player, this.layer);
-        this.player.animations.play('run');
+
+        // Deaccelerates player every frame
+        this.player.body.acceleration.x = 0;
+        this.player.body.drag.x = this.player.body.velocity.x / 1.5;
+        
+        if (this.player.body.velocity.x < 10) {
+            this.player.frame = 0;  
+        };
 	},
     
     createWorld: function () {
         this.map = game.add.tilemap('pista1');
         this.map.addTilesetImage('pista1_tileset');
         this.layer = this.map.createLayer('pista1');
+        
         //this.layer.resizeWorld();
-        // TODO: ver si hace falta esto para los tilemaps
+        // TODO: check if above line is necessary
         this.map.setCollision(2);
         game.world.setBounds(0, 0, 1280, 320);
+    },
+    
+    movePlayer: function () {
+        this.player.animations.play('run');
+
+        // This value should be replaced with the value fetched from the aimbar
+        this.player.body.acceleration.x = 5000;
     },
 };
