1615d3268f7979b08e3046d1c8f97bb025106f98
Facundo Estevez <fmestevez@gmail.com>
2014-08-27 16:01:05 -0300

    Facundo Estevez - PODVI - Create new tilemap & tileset, enable physics

--- public/js/play.js
+++ public/js/play.js
@@ -6,9 +6,23 @@ var playState = {
         this.player.animations.add('run', [0, 1], 8, true);
         this.player.animations.add('jump', [2, 3], 8, true);
         game.physics.arcade.enable(this.player);
+        this.player.body.gravity.y = 400;
+        
+        this.createWorld();
 	},
 
 	update: function() {
+        game.physics.arcade.collide(this.player, this.layer);
         this.player.animations.play('run');
 	},
+    
+    createWorld: function () {
+        this.map = game.add.tilemap('pista1');
+        this.map.addTilesetImage('pista1_tileset');
+        this.layer = this.map.createLayer('pista1');
+        //this.layer.resizeWorld();
+        // TODO: ver si hace falta esto para los tilemaps
+        this.map.setCollision(2);
+        game.world.setBounds(0, 0, 1280, 320);
+    },
 };
