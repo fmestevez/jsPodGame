a27ad31fda58274c8dcada87ed14fbeeb755b002
Marthinoth <marthinoth@gmail.com>
2014-08-29 02:18:36 -0300

    Martin - Added mouse click button - Removed spaces - Formatted some lines

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,16 +1,16 @@
 var menuState = {
 
-	create: function() { 
+	create: function() {
 		// Name of the game
-		var nameLabel = game.add.text(game.world.centerX, 80, 'Name', 
+		var nameLabel = game.add.text(game.world.centerX, 80, 'Name',
             { font: '50px Arial', fill: '#ffffff' });
 		nameLabel.anchor.setTo(0.5, 0.5);
 
 		// How to start the game
-		var startLabel = game.add.text(game.world.centerX, game.world.height-80, 
+		var startLabel = game.add.text(game.world.centerX, game.world.height - 80,
             'press the spacebar to start', { font: '25px Arial', fill: '#ffffff' });
-		startLabel.anchor.setTo(0.5, 0.5);	
-		game.add.tween(startLabel).to({angle: -2}, 500).to({angle:2}, 500).loop().start(); 
+		startLabel.anchor.setTo(0.5, 0.5);
+		game.add.tween(startLabel).to({angle: -2}, 500).to({angle: 2}, 500).loop().start();
 
 		// Add a mute button
 		this.muteButton = game.add.button(20, 20, 'mute', this.toggleSound, this);
@@ -22,14 +22,16 @@ var menuState = {
 		// Start the game when the up arrow key is pressed
 		var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.addOnce(this.start, this);
+        // Start the game when the mouse button is pressed
+        game.input.onDown.addOnce(this.start, this);
 	},
 
 	toggleSound: function() {
-		game.sound.mute = ! game.sound.mute;
-		this.muteButton.frame = game.sound.mute ? 1 : 0;	
+		game.sound.mute = !game.sound.mute;
+		this.muteButton.frame = game.sound.mute ? 1 : 0;
 	},
 
 	start: function() {
-		game.state.start('play');	
+		game.state.start('play');
 	}
 };
